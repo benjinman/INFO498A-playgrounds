e1b173b87f865c2269064305a1f492bfd645bb82
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-11 18:27:58 -0700

    updated

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,7 +6,7 @@ var str = "Hello, playground"
 
 struct Money {
     
-    private var pennies : Int32
+    private var pennies : Int32 = 0
     var amount : Int32 {
         get {
             return pennies / 100
@@ -16,23 +16,56 @@ struct Money {
         }
     }
     
+    init(amt : Int32 = 0) {
+        self.amount = amt
+    }
+    
+    /*
     var currency : String
     
     var frozen : Bool {
         get {
             return false
         }
-    }
+    }*/
 }
 
-class Currency {
+class Person {
     
+    var firstName : String
+    var lastName : String
+    var age : UInt8 = 0
+    
+    static var totalPopulation = 0;
+    func printPopulation() {
+        print("There are currently \(Person.totalPopulation) persons in existence")
+    }
+    
+    init(fn: String, ln: String) {
+        firstName = fn
+        lastName = ln
+        Person.totalPopulation += 1
+    }
+    
+    convenience init(fn: String, ln: String, a: UInt8) {
+        self.init(fn: fn, ln: ln)
+        self.age = a
+    }
+    
+    func sayHowdy() {
+        print("\(firstName) says hello")
+    }
+    
+    func getOlder() {
+        print("\(firstName) has a birthday! Wheeee!")
+        self.age += 1
+    }
 }
 
 // Never use floats to represent currency
 
-let m = Money(currency: "USD")
+let m = Money()
 let m2 = m
 
-let c = Currency()
-let c2 = c
+let p = Person(fn: "Ben", ln: "Jin")
+p.sayHowdy()
